6e5cd08e37490a1d8ea9e11ecbf0fe2eb4eccd1d
Thuwarakan <thuwarakan123@gmail.com>
2024-01-05 22:50:46 +0530

    Creating routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
 const express = require('express');
+const workoutRoute = require("./routes/workout")
 
 const app = express();
+
+app.use(express.json())
 
 // middleware
 app.use((req, res, next) => {
@@ -10,9 +13,8 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+// routes
+app.use('/api/workouts', workoutRoute);
   
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT)
