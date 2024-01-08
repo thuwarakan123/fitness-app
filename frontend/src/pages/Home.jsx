d5b42c98edf3691e5b20984fa25b939bd80f8ba5
Thuwarakan <thuwarakan123@gmail.com>
2024-01-08 22:34:25 +0530

    Fetch data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,31 @@
-const Home = () => {
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
 
-    return (
-      <div className="home">
-        <h2>Home</h2>
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+  
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
       </div>
-    )
+    </div>
+  )
 }
   
 export default Home
